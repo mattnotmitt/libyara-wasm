2338af1980cb23ad42a25940b37eee789033615c
Matt <matt@artemisbot.uk>
2019-01-06 22:04:07 +0000

    Currently non-functional

--- main.cpp
+++ main.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <yara.h>
-#include "stdio.h"
+#include <emscripten/bind.h>
 #include "util.hpp"
 
-int main() {
-    yr_initialize();
-
+std::map<std::string, std::vector<resolved_match>> run(const std::string &buf_str, const std::string &rules_str) {
     YR_RULES* rules;
-    YR_RULE* rule;
+    std::map<std::string, std::vector<resolved_match>> matched_rules;
 
-    std::vector<std::string> matches;
-    char* buf = "foo bar";
-    char* rules_str = "rule true_rule { condition: true }";
+    char* buf = const_cast<char*>(buf_str.c_str());
+    char* rules_chr = const_cast<char*>(rules_str.c_str());
 
     yr_initialize();
-    int result = compile_rule(rules_str, &rules);
+    int result = compile_rule(rules_chr, &rules);
     if (result != ERROR_SUCCESS)
     {
         fprintf(stderr, "failed to compile rule: %s\n", compile_error);
         perror("compile_rule");
         exit(EXIT_FAILURE);
     }
-    yr_rules_scan_mem(rules, (uint8_t *) buf, strlen(buf), 0, count_matches, &matches, 0);
-    std::cout << "Total matches: " << matches.size() << std::endl << "Matched rules:" << std::endl;
-    for (auto const& match : matches) {
-        std::cout << match << std::endl;
-    }
+    yr_rules_scan_mem(rules, (uint8_t *) buf, strlen(buf), 0, get_matched_rules, &matched_rules, 0);
     yr_finalize();
-    return 0;
+    std::cout << matched_rules["bar"][0].location << std::endl;
+    return matched_rules;
 }
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    emscripten::value_object<resolved_match>("resolved_match")
+            .field("location", &resolved_match::location)
+            .field("match_length", &resolved_match::match_length)
+            .field("data", &resolved_match::data);
+    emscripten::register_vector<resolved_match>("vector<resolved_match>");
+    emscripten::register_map<std::string, std::vector<resolved_match>>("map<string, vector<resolved_match>>");
+    emscripten::function("run", &run);
+}
+
--- util.hpp
+++ util.hpp
@@ -30,8 +30,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef _UTIL_H
 #define _UTIL_H
 
-#include <iostream>
+#include <map>
 #include <vector>
+#include <string>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -40,7 +41,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <fcntl.h>
 
 #include <yara.h>
-
+struct resolved_match {
+    int64_t location;
+    int32_t match_length;
+    std::string data;
+};
 extern char compile_error[1024];
 extern int warnings;
 
@@ -49,196 +54,9 @@ int compile_rule(
         YR_RULES** rules);
 
 
-int count_matches(
+int get_matched_rules(
         int message,
         void* message_data,
         void* user_data);
-
-
-int do_nothing(
-        int message,
-        void* message_data,
-        void* user_data);
-
-
-int matches_blob(
-        char* rule,
-        uint8_t* blob,
-        size_t len);
-
-
-int matches_string(
-        char* rule,
-        char* string);
-
-
-int capture_string(
-        char* rule,
-        char* string,
-        char* expected_string);
-
-
-int read_file(
-        char* filename, char** buf);
-
-
-#define assert_true_expr(expr)                                          \
-  do {                                                                  \
-    if (!(expr)) {                                                      \
-      fprintf(stderr, "%s:%d: expression is not true\n",                \
-              __FILE__, __LINE__ );                                     \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-
-#define assert_true_rule(rule, string)                                  \
-  do {                                                                  \
-    if (!matches_string(rule, string)) {                                \
-      fprintf(stderr, "%s:%d: rule does not match (but should)\n",      \
-              __FILE__, __LINE__ );                                     \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-#define assert_true_rule_blob_size(rule, blob, size)                    \
-  do {                                                                  \
-    if (!matches_blob(rule, (uint8_t*) (blob), size)) {                 \
-      fprintf(stderr, "%s:%d: rule does not match (but should)\n",      \
-              __FILE__, __LINE__ );                                     \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-#define assert_true_rule_blob(rule, blob)               \
-  assert_true_rule_blob_size(rule, blob, sizeof(blob))
-
-#define assert_true_rule_file(rule, filename)                           \
-  do {                                                                  \
-    char* buf;                                                          \
-    size_t sz;                                                          \
-    if ((sz = read_file(filename, &buf)) == -1) {                       \
-      fprintf(stderr, "%s:%d: cannot read file '%s'\n",                 \
-              __FILE__, __LINE__, filename);                            \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-    if (!matches_blob(rule, (uint8_t*) (buf), sz)) {                    \
-      fprintf(stderr, "%s:%d: rule does not match contents of"          \
-              "'%s' (but should)\n",                                    \
-              __FILE__, __LINE__, filename);                            \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-    free(buf);                                                          \
-  } while (0);
-
-#define assert_false_rule(rule, string)                                 \
-  do {                                                                  \
-    if (matches_string(rule, string)) {                                 \
-      fprintf(stderr, "%s:%d: rule matches (but shouldn't)\n",          \
-              __FILE__, __LINE__ );                                     \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-#define assert_false_rule_blob_size(rule, blob, size)                   \
-  do {                                                                  \
-    if (matches_blob(rule, (uint8_t*) (blob), size)) {                  \
-      fprintf(stderr, "%s:%d: rule matches (but shouldn't)\n",          \
-              __FILE__, __LINE__ );                                     \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-#define assert_false_rule_blob(rule, blob)              \
-  assert_false_rule_blob_size(rule, blob, sizeof(blob))
-
-#define assert_false_rule_file(rule, filename)                          \
-  do {                                                                  \
-    char* buf;                                                          \
-    size_t sz;                                                          \
-    if ((sz = read_file(filename, &buf)) == -1) {                       \
-      fprintf(stderr, "%s:%d: cannot read file '%s'\n",                 \
-              __FILE__, __LINE__, filename);                            \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-    if (matches_blob(rule, (uint8_t*) (buf), sz)) {                     \
-      fprintf(stderr, "%s:%d: rule matches contents of"                 \
-              "'%s' (but shouldn't)\n",                                 \
-              __FILE__, __LINE__, filename);                            \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-    free(buf);                                                          \
-  } while (0);
-
-#define assert_error(rule, error) do {                                  \
-    YR_RULES* rules;                                                    \
-    int result = compile_rule(rule, &rules);                            \
-    if (result == ERROR_SUCCESS)                                        \
-      yr_rules_destroy(rules);                                          \
-    if (result != error) {                                              \
-      fprintf(stderr, "%s:%d: expecting error %d but returned %d\n",    \
-              __FILE__, __LINE__, error, result);                       \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-
-#define assert_warnings(rule, w) do {                                   \
-    YR_RULES* rules;                                                    \
-    int result = compile_rule(rule, &rules);                            \
-    if (result == ERROR_SUCCESS) {                                      \
-      yr_rules_destroy(rules);                                          \
-      if (warnings < w) {                                               \
-        fprintf(stderr, "%s:%d: expecting warning\n",                   \
-                __FILE__, __LINE__);                                    \
-        exit(EXIT_FAILURE);                                             \
-      }                                                                 \
-    }                                                                   \
-    else {                                                              \
-      fprintf(stderr, "%s:%d: failed to compile << %s >>: %s\n",        \
-              __FILE__, __LINE__, rule, compile_error);                 \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-
-#define assert_no_warnings(rule) do {                                   \
-    YR_RULES* rules;                                                    \
-    int result = compile_rule(rule, &rules);                            \
-    if (result == ERROR_SUCCESS) {                                      \
-      yr_rules_destroy(rules);                                          \
-      if (warnings > 0) {                                               \
-        fprintf(stderr, "%s:%d: unexpected warning\n",                  \
-                __FILE__, __LINE__);                                    \
-        exit(EXIT_FAILURE);                                             \
-      }                                                                 \
-    }                                                                   \
-    else {                                                              \
-      fprintf(stderr, "%s:%d: failed to compile << %s >>: %s\n",        \
-              __FILE__, __LINE__, rule, compile_error);                 \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-
-#define assert_warning(rule) assert_warnings(rule, 1)
-
-
-#define assert_true_regexp(regexp,string,expected) do {                 \
-    if (!capture_string("rule test { strings: $a = /" regexp            \
-                        "/ condition: $a }", string, expected)) {       \
-      fprintf(stderr, "%s:%d: regexp does not match\n",                 \
-              __FILE__, __LINE__);                                      \
-      exit(EXIT_FAILURE);                                               \
-    }                                                                   \
-  } while (0);
-
-#define assert_false_regexp(regexp,string)                              \
-  assert_false_rule("rule test { strings: $a = /" regexp                \
-                    "/ condition: $a }", string)
-
-#define assert_regexp_syntax_error(regexp)                              \
-  assert_error("rule test { strings: $a = /" regexp "/ condition: $a }",\
-               ERROR_INVALID_REGULAR_EXPRESSION)
 
 #endif /* _UTIL_H */
